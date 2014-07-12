b089783be2c85fc1208e275e6adda284c64078fe
iker <iker.azpeitia@ehu.es>
2014-07-12 16:56:53 +0200

    Update ld.js

--- iker/ld.js
+++ iker/ld.js
@@ -514,8 +514,7 @@ rdf = (function() {
  }
     
   ins.prototype.addProperty = function (property, values, processor){
-  	y.log('este es: '+values);
-	if (typeof processor == 'undefined' ){
+  	if (typeof processor == 'undefined' ){
 		var processor= function (value){return value;};
 	}
 	if ( Object.prototype.toString.call(values) == '[object String]'){
