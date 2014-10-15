f077a06d75f822043249aa89d7a79e6f15df1813
iker <iker.azpeitia@ehu.es>
2014-10-15 15:18:12 +0200

    Update jsonld.js

--- jsonld.js
+++ jsonld.js
@@ -34,7 +34,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 (function() {
-
+y.log ('buuuuuu');
 // determine if in-browser or using node.js
 var _nodejs = (
   typeof process !== 'undefined' && process.versions && process.versions.node);
